ad486168688756f02504025d0c5fe9addd55957c
islamjameel <islam.jameel9@gmail.com>
2018-05-28 22:46:00 +0300

    *6* edit CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,6 +18,7 @@
 document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
   tinymce.init({ 
+    height: '350',
     selector:'textarea#post_body',
     plugins: "codesample image media link code",
     toolbar: "undo redo | styleselect | bold italic link | codesample image media code" });
